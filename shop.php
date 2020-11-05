79abe9019d6dd5df07ddb08764bf8de931bd4793
Aliakbar Sadeghy <aliakbar.sadeghy@gmail.com>
2020-11-05 18:39:24 +0330

    Add navbar

---
+++ shop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+</head>
+<body>
+      
+      <h1>Welcome to shop page</h1>
+
+      <nav><a href="#">about</a><a href="#">contact us</a></nav>
+
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo veniam quod incidunt veritatis, illum quos corrupti est mollitia exercitationem repellendus ut quas aperiam ipsam provident fugit aut officia voluptate.</p>
+
+      <script src="js/main.js"></script>
+
+</body>
+</html>
